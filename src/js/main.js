f129183caac2a0c674eaeb96ea005a8c53b90676
rouanis5 <ouanis42@gmail.com>
2021-08-24 22:35:26 +0100

    animation on scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -478,3 +478,27 @@ for (let i = 0; i < sectionsBtn.length; i++) {
         }
     };
 }
+//create fade-in animation
+const faders = document.querySelectorAll(".fade-in");
+const appearOptions = {
+    threshold: 0.5,
+    // rootMargin: "0px 0px 45px 0px",
+};
+const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach((fader) => {
+    appearOnScroll.observe(fader);
+});
+const sliders = document.querySelectorAll(".slide-in");
+sliders.forEach((slider) => {
+    appearOnScroll.observe(slider);
+});
