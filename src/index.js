1505792ba9a349a840348533db4143806a946351
rouanis5 <ouanis42@gmail.com>
2022-02-22 16:09:53 +0100

    update to parcel 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
-import "../node_modules/normalize.css/normalize.css";
+import "npm:@fortawesome/fontawesome-free/css/all.min.css";
+import "npm:normalize.css/normalize.css";
 import "./scss/style.scss";
 import "./js/main.js";
 import "./js/increaseNums.js";
